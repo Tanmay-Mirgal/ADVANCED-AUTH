639980fa9d9c66aa6f5f920d8e613331f9a66288
Tanmay-Mirgal <tanmaymirgal26@gmail.com>
2024-12-14 17:22:12 +0530

    backend done

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { login, logout, signup ,verifyEmail, forgotPassword,resetPassword} from '../controllers/auth.controller.js';
+import { login, logout, signup ,verifyEmail, forgotPassword,resetPassword, checkAuth} from '../controllers/auth.controller.js';
+import {verifyToken}  from '../middlewares/verifyToken.js';
 const router = Router();
 
-
+router.get('/check-auth',verifyToken, checkAuth)
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout', logout);
--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './db.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const app = express();
 app.use(express.json());//allows us to parse json data
+app.use(cookieParser());//allows us to parse incomming cookies 
 app.use(express.urlencoded({extended:true}));
 
 app.use('/api/auth',authRoutes);
